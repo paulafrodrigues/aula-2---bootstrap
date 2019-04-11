79e3104c849b2b0685f7081635f825d85e54a325
Paula F Rodrigues <43822859+paulafrodrigues@users.noreply.github.com>
2019-04-11 13:20:50 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,44 @@
+let nome = document.querySelector("#nome");
+let email = document.querySelector("#email");
+let telefone = document.querySelector("#tel");
+let mensagem = document.querySelector("#mensagem");
+
+const botao = document.querySelector(".btn-primary");
+
+botao.addEventListener('click', function(evento){
+    evento.preventDefault();
+    if (nome.value=== undefined || 
+        nome.value=== null ||
+        nome.value.trim()=== "" || 
+        nome.value.length<0){
+        nome.focus();
+        alert('Preencha o campo nome corretamente!')
+        return false;
+        }else if 
+            (email.value=== undefined || 
+             email.value=== null ||
+             email.value.trim()=== "" || 
+             email.value.length<0){
+             email.focus();
+             alert('Preencha o campo e-mail corretamente!')
+             return false;
+        } else if
+            (telefone.value=== undefined || 
+            telefone.value=== null ||
+            telefone.value.trim()=== "" || 
+            telefone.value.length<11){
+            telefone.focus();
+            alert('Preencha o campo de telefone corretamente!')
+            return false;         
+    }else if
+            (mensagem.value=== undefined ||
+            mensagem.value=== null ||
+            mensagem.value.trim()=== "" ||
+            mensagem.value.length>=10){
+            mensagem.focus();
+            alert('Preencha a sua mensagem!')
+            return false;
+            }
+    document.querySelector('form').submit()
+    alert('Orçamento enviado com sucesso')
+})
